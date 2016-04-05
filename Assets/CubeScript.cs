24773b27c1c587b491e59ff5a2508a390459ecc1
Hunter Waite-144002153 <144002153@vusdnoc.vusd.k12.ca.us>
2016-04-05 13:36:12 -0700

    Initial Commit

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x+1, 0, 0);
+	}
+}
